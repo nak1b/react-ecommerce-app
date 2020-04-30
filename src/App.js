c1707acc1fd6701872b7b1259b0518bc2d9e5a4e
Nakib Momin <nbmomin14@gmail.com>
2020-04-29 22:28:08 -0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import './App.css'
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsComponent = () => (
+  <div>
+    <h1>Hats Component</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatsComponent} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

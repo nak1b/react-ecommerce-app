36115a6dae76085c24a340a07ca4bf41676840c2
Nakib Momin <nbmomin14@gmail.com>
2020-05-16 23:20:56 -0400

    added redux and implemented user auth action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import './App.css'
 import Header from './components/header/header.component'
@@ -7,29 +9,24 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.components'
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  state = {
-    currentUser: null
-  }
-
   unsubscribeFromAuth = null 
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(!userAuth) {
-        this.setState({ currentUser: userAuth })
+        this._setUser(userAuth)
         return 
       }
       
       const userRef = await createUserProfileDocument(userAuth)
 
       userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
-            id: snapShot.id,
-            ...snapShot.data()
-          }
+        this._setUser({
+          id: snapShot.id, 
+          ...snapShot.data() 
         })
       })
     })
@@ -39,10 +36,14 @@ class App extends React.Component {
     this.unsubscribeFromAuth()
   }
 
+  _setUser = (user) => {
+    this.props.setCurrentUser(user)
+  }
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -53,4 +54,10 @@ class App extends React.Component {
   }
 }
 
-export default App
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    setCurrentUser
+  }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(App)
